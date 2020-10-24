066cb369bcf6c3c1fa0eda020d7a7240a48eaaac
matheus <matheuslima20111997@gmail.com>
2020-10-23 23:17:45 -0300

    conectando ao servidor em node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react'
+import Cabecalho from './components/Cabecalho'
+import Rodape from './components/Rodape'
+import './App.css'
+import api from './services/api'
 
 function App() {
+
+  const [projetos,setProjetos] = useState([])
+  //no useState fica o valor padrao e ele retorna o valor da variavel e uma duncao pra atualizar
+
+  useEffect(()=>{
+    api.get('projetos').then(dados=>{
+      setProjetos(dados.data)
+    })
+  },[])
+
+  function adcProjeto(){
+    setProjetos([...projetos,`cat ${Date.now()}`])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Cabecalho titulo="Hello react">
+        <ul>
+          {projetos.map((projeto,index) => {
+            return (<li key={index}>{`${projeto.valor} - ${projeto.nome}`}</li>)
+          })}
+        </ul>
+      </Cabecalho>
+
+      <button type="button" onClick={adcProjeto}>Nova</button>   
+     
+      <Rodape empresa="Volpi" />
+    </>
+  )
 }
 
 export default App;
